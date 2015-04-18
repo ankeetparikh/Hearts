f5ce87a2d91709df4d6a1f9275468e7bbbcf55ec
Ankeet Parikh <ankeet@nbp-180-57.nbp.ruw.rutgers.edu>
2015-04-18 14:33:19 -0400

    Player Constructor

---
+++ GamePlay.py
@@ -0,0 +1,22 @@
+import random;
+
+Deck = range(52);
+random.shuffle(Deck);
+
+class Player:
+	clubs = []
+	spades = [];
+	diamonds = []
+	hearts = []
+	def __init__(self, clubs, spades, diamonds, hearts):
+		self.clubs = clubs;
+		self.hearts = hearts;
+		self.spades = spades;
+		self.hearts = hearts;
+	
+	def playCard():
+		#implementation goes here
+	def pass():
+				 
+	
+print Deck;

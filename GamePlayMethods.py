efe37d02507cb6b795c329b60e6540fe2910e50b
Ankeet Parikh <ankeet@nbp-180-57.nbp.ruw.rutgers.edu>
2015-04-18 18:05:40 -0400

    stuff

--- GamePlayMethods.py
+++ GamePlayMethods.py
@@ -8,10 +8,10 @@
 '''
 import random;
 
-Deck = [];
-
+'''
 class Card:
 	#implement here
+'''
 class Player:
 	points = 0;
 	clubs = [];
@@ -26,11 +26,12 @@ class Player:
 		self.diamonds = hand['diamonds'];
 		self.spades = hand['spades'];
 		self.hearts = hand['hearts'];
-			
+	'''		
 	def playCard():
 		#implementation goes here
 	def pass():
 		#more implementation
+	'''
 '''
 parameter: the list of player objects (length must be 4)
 return: void
@@ -39,10 +40,11 @@ Changes the hand of each player in playerlist
 '''
 def Deal(playerList):
 	Deck = range(52);
-	Deck.shuffle();
+	random.shuffle(Deck);
 	p1,p2,p3,p4 = Deck[0:13], Deck[13:26], Deck[26:39], Deck[39:52];
 	hands = [p1,p2,p3,p4];				 
-	for currhand in hands:
+	for i in range(4):
+		currhand = hands[i];
 		a = currhand.sort();
 		toAssign = {};
 		toAssign['spades']  =[];
@@ -52,17 +54,22 @@ def Deal(playerList):
 		for j in currhand:
 			suit = j/13;
 			card = j%13;
-			if(suit == 0)
+			if(suit == 0):
 				toAssign['clubs'].append(card);
-			if(suit == 1)
+			if(suit == 1):
 				toAssign['diamonds'].append(card);
-			if(suit == 2)
+			if(suit == 2):
 				toAssign['spades'].append(card);
-			if(suit == 3)
+			if(suit == 3):
 				toAssign['hearts'].append(card);
-				
-	
-print Deck;
+		print toAssign
+		#playerList[i].setHand(toAssign);
+a = Player();
+b = Player();
+c = Player();
+d = Player();
+Deal([a,b,c,d]);
+print a.clubs;			
 
 
 

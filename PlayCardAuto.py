fede0138b583b12815f0524516a2a4c83b99886a
nitikayad96 <nitikayad96@gmail.com>
2015-04-18 21:43:37 -0400

    playcard/passcard methods

--- PlayCardAuto.py
+++ PlayCardAuto.py
@@ -10,16 +10,24 @@ class PlayCardAuto:
 			play = Player.hand(r)
 			Player.hand.pop(r)
 		elif len(sofar) > 0:
-			lead_card = sofar(1)
+			lead_card = sofar[0]
 			lead_suit = lead_card/13
 			for i in Player.hand:
+				options = []
 				if i/13 == lead_suit:
-					play = i
-					Player.hand.remove(i)
-					break
+					options.append(i)
+				if len(options)>0:
+					play = options[randint(0,len(options)-1)]
+					Player.hand.remove(play)
 			play = Player.hand(r)
 			Player.hand.pop(r)
 		
 		return play
+
+	def passCard(Player):
+		a = Player.hand.pop()
+		b = Player.hand.pop()
+		c = Player.hand.pop()
+		return [a,b,c]
 
 
